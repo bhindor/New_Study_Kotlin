c30e3c25de4bb517166ebb643fdc0f2bdeb2ea27
bhindor <bhindor@dgsw.hs.kr>
2022-11-24 09:12:38 +0900

    mvvm_example
    mvvm_example
    2022/11/24

---
+++ mvvm_example/app/src/main/java/com/example/mvvm_example/view/OrderActivity.kt
@@ -0,0 +1,19 @@
+package com.example.mvvm_example.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.example.mvvm_example.R
+
+class OrderActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_order)
+<<<<<<< HEAD
+        
+=======
+
+        Log.d("test", "test")
+>>>>>>> main
+    }
+}
---
+++ mvvm_example/app/src/main/java/com/example/mvvm_example/viewmodel/OrderViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.mvvm_example.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class OrderViewModel : ViewModel() {
+    
+}
