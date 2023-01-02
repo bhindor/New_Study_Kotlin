07ca77cd2b58cb0828ddf2fc19909d6262de711f
bhindor <bhindor@dgsw.hs.kr>
2023-01-02 15:38:17 +0900

    🧩 : ContactWithMVVM
    2023/01/02

---
+++ ContactMVVM/app/src/main/java/com/example/contactwithmvvm/ContactDao.kt
@@ -0,0 +1,21 @@
+package com.example.contactwithmvvm
+
+import android.arch.persistence.room.Dao
+import android.arch.persistence.room.Delete
+import android.arch.persistence.room.Insert
+import android.arch.persistence.room.OnConflictStrategy
+import android.arch.persistence.room.Query
+import androidx.lifecycle.LiveData
+
+@Dao
+interface ContactDao {
+
+    @Query("SELECT * FROM contact ORDER BY name ASC")
+    fun getAll(): LiveData<List<Contact>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(contact: Contact)
+
+    @Delete
+    fun delete(contact: Contact)
+}
