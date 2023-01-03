99c3c60c7b1be38f1f7a24e479878acc643dbd5b
bhindor <bhindor@dgsw.hs.kr>
2023-01-03 17:06:19 +0900

    Modify : MainActivity
    2023/01/03

--- ContactMVVM/app/src/main/java/com/example/contactwithmvvm/MainActivity.kt
+++ ContactMVVM/app/src/main/java/com/example/contactwithmvvm/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.contactwithmvvm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var contactViewModel: ContactViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        contactViewModel = ViewModelProvider(this).get(ContactViewModel::class.java)
+        contactViewModel.getAll().observe(this, Observer<List<Contact>> { contacts ->
+            // Update UI
+        })
     }
 }
